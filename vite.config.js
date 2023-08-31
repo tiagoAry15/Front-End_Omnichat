bf7abc7e60ea64e7fbbec2387e3b76ce638c6992
Tiago <tiago.ary10@gmail.com>
2023-08-31 17:52:27 -0300

    config analytics

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
+  },
+  build: {
+    rollupOptions: {
+      external: ['@vercel/analytics/react']
+    }
   }
 })
+
