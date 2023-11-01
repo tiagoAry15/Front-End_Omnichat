45119f5bb9087c59ffedcae1210c70954f71c1e4
Tiago <tiago.ary10@gmail.com>
2023-11-01 16:17:20 -0300

    adicionando firebase_SK (sem credencial)

---
+++ firebase_SDK.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+   apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_APP_DATABASEURL,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_APPID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENTID
+};
