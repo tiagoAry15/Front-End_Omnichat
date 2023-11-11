0693c2762f76e9a6e73e00e8710416953392dcf3
Luiz Miguel <luizmgc1930@gmail.com>
2023-11-11 00:04:08 -0300

    update

--- src/pages/Orders/OrderContent.jsx
+++ src/pages/Orders/OrderContent.jsx
@@ -1,16 +1,16 @@
 // OrderScreenContent.jsx
 import React, { useContext } from 'react';
-import { Accordion, AccordionItem, AccordionHeader, AccordionBody, Container, Button, Input, Form, Label, FormGroup } from "reactstrap";
+import { Accordion, AccordionItem, AccordionHeader, AccordionBody, Card, CardBody, CardTitle, CardSubtitle, CardText,Container, Button, Input, Form, Label, FormGroup } from "reactstrap";
 import { ToastContainer } from 'react-toastify';
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 import PlatformIcon from './PopUpIcon';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './orderCard.css'
 import { OrderContext } from '../../contexts/OrderContext';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, withTranslation } from 'react-i18next';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-
-const OrderContent = () => {
+const OrderContent = (props) => {
 
   const {
     deletingOrderId,
@@ -49,16 +49,20 @@ const OrderContent = () => {
     <div className="page-content">
       <Container fluid={true}>
         <Breadcrumbs title='Omnichat' breadcrumbItem={t("Order")} />
-        <div style={styles.container} className='right'>
+        <div className='right orderContainer'>
           {orders.map((order, index) => (
-            <div key={orderKeys[index]} style={styles.card} className={`order-item ${deletingOrderId === orderKeys[index] ? 'up' : ''}`}>
-              <div style={styles.container_between}>
-                <h3 style={styles.customerName}>{index + 1}: {order.customerName}</h3>
+            <Card style={{ width: '18rem'}}>
+            <CardBody>
+              <CardTitle tag="h5">
+              <div className='container_between'>
+                <h3 className='customerName'>{index + 1}: {order.customerName}</h3>
                 <PlatformIcon platform={order.platform} communication={order.communication} />
               </div>
-              <p style={styles.pizza}>{order.pizza}</p>
-              <p className="observation-field"><p><strong>Detalhes do pedido:</strong>{buildOrderItemsText(order.orderItems)}<strong>Observação:</strong></p>{order.observation}</p>
-              <div style={styles.container_between}>
+              </CardTitle>
+              <p className='pizza'>{order.pizza}</p>
+              <p className="observation-field"><p>
+                <strong>Detalhes do pedido:</strong>{buildOrderItemsText(order.orderItems)}<strong>Observação:</strong></p>{order.observation}</p>
+              <div className='container_between'>
                 <select
                   value={selectedOptions[orderKeys[index]] || ''}
                   onChange={(event) => handleSelectChange(event, orderKeys[index])}
@@ -150,50 +154,15 @@ const OrderContent = () => {
                   </AccordionBody>
                 </AccordionItem>
               </Accordion>
-            </div>
-
+            </CardBody>
+          </Card>
           ))}
           <ToastContainer />
         </div>
+        
       </Container>
     </div>
   );
 }
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'start',
-    overflowX: 'auto',
-    padding: '20px',
-  },
-  container_between: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  card: {
-    backgroundColor: '#fff',
-    borderRadius: '8px',
-    margin: '10px',
-    padding: '20px',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '300px',
-    height: '100%'
-  },
-  customerName: {
-    fontSize: '18px',
-    fontWeight: 'bold',
-    marginBottom: '10px',
-  },
-  pizza: {
-    fontSize: '16px',
-  },
-
-};
-
-export default OrderContent;
+export default withRouter(withTranslation()(OrderContent));
